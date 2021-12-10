b1369b386165a37746072071e6a6ab9eaa951a2f
Sergio Pedro <seperool@gmail.com>
2021-12-10 16:40:18 -0300

    comentando pacotes - 1

--- Cap3_Pacotes.R
+++ Cap3_Pacotes.R
@@ -1,7 +1,13 @@
-install.packages("rmarkdown")
-install.packages("knitr")
-install.packages("data.table")
-install.packages("janitor")
-install.packages("DescTools")
-install.packages("tidyverse")
-install.packages("foreign")
+install.packages("rmarkdown") #pacote para fazer relatorio, rmd
+install.packages("knitr") #
+install.packages("data.table") #
+install.packages("janitor") #
+install.packages("DescTools") #
+install.packages("tidyverse") #Conjunto de pacotes para leitura, manipulação, exploração e compartilhamento de dados
+#ggplot2 (visualização)
+#tibble (tipo de data.frame) 
+#tidyr (organização de dados)
+#readr (leitura de dados)
+#purrr (programação funcional)
+#dplyr (manipulação de dados)
+install.packages("foreign") #leitura de arquivos com extensão minitab, Octave, ...
