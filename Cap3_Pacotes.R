5a93aa0e3979d4e0912dc9c1cc9b76f3e4f7f179
Sergio Pedro <seperool@gmail.com>
2021-12-25 22:12:56 -0300

    atualização de pacotes - tidyr

--- Cap3_Pacotes.R
+++ Cap3_Pacotes.R
@@ -3,6 +3,9 @@ install.packages("knitr") #
 install.packages("data.table") #
 install.packages("janitor") #
 install.packages("DescTools") #
+
+devtools::install_github("hadley/devtools") #quebrado achar equivalente
+
 install.packages("tidyverse") #Conjunto de pacotes para leitura, manipulação, exploração e compartilhamento de dados
 #ggplot2 (visualização)
 #tibble (tipo de data.frame) 
@@ -11,3 +14,11 @@ install.packages("tidyverse") #Conjunto de pacotes para leitura, manipulação, 
 #purrr (programação funcional)
 #dplyr (manipulação de dados)
 install.packages("foreign") #leitura de arquivos com extensão minitab, Octave, ...
+install.packages("tidyr") #pacote auxiliará a organização dos dados, tendo duas propriedades:
+#cada coluna é uma variavel
+#cada linha é uma observação
+
+#não ta funcionando, testar mais devtools
+
+#obs.: mais seguro, baixar a versão do github, por ser a mais atualizada
+devtools::install_github("tidyverse/tidyr")
