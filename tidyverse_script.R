f42c7e3b3323d59be2c701f83502252e77994290
Sergio Pedro <seperool@gmail.com>
2021-12-29 09:08:11 -0300

    tidyr - organização de dados - unite

--- tidyverse_script.R
+++ tidyverse_script.R
@@ -606,3 +606,29 @@ respostas
 respostas %>%
   separate(cor, c("cor1","cor2"), sep = ",")
 #note que o separador para a resposta cor é a virgula.
+
+#---
+#tidyr
+#Organização de dados
+#separate & unite
+
+#unite
+
+#o comando unite é utilizado para unir duas ou mais colunas.
+
+library(magrittr) #operador pipe %>%, concatena linhas de comando
+library(tidyr) #organização de dados
+
+#a tabela inicial de dados
+
+respostas = data.frame(ordem = 1:6,
+                       cor = c("azul, amarelo","verde, cinza","azul, verde","azul, cinza","branco, preto","verde, cinza"))
+
+resposta_sep = respostas %>%
+  separate(cor, c("cor1","cor2"), sep = ",")
+resposta_sep
+
+#unindo respostas sobre cores
+
+resposta_sep %>%
+  unite("cor", c("cor1","cor2"),sep = ",")
