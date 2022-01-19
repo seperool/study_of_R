2ebd35e895c9be63726c29461eca620c9f08d0d2
Sergio Pedro <seperool@gmail.com>
2022-01-19 12:58:47 -0300

    dplyr - agrupa dados de uma determinada coluna ou colunas - exemplo 1

---
+++ Cap5_dplyr-group_by_part1.R
@@ -0,0 +1,23 @@
+# Importação de dados pelo import dataset
+View(vendas1)
+
+# Leitura de dados de arquivo em URL
+dados = read.csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados)
+dados1 = readr::read_csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados1)
+dados2 = read.csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados2)
+dados3 = readr::read_csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados3)
+
+#dplyr
+#Manipulando dados com o dplyr
+#Group_by
+  #para agruparmos dados
+#observando o numero de cupons distintos por cada filial
+library(magrittr)
+library(dplyr)
+dados1 %>%
+  group_by(filial)%>%
+  summarise(cupons_distintos = n_distinct(cupom))
