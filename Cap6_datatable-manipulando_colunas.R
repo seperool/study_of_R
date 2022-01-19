3a2d627c85d992f50b9a4e796970ccbad5bee3d5
Sergio Pedro <seperool@gmail.com>
2022-01-18 21:18:09 -0300

    data.table - 8 formas de manipular colunas - exemplos

--- Cap6_datatable-manipulando_colunas.R
+++ Cap6_datatable-manipulando_colunas.R
@@ -38,4 +38,32 @@ dados <- read.csv2(file = "http://raw.githubusercontent.com/Lucianea/Alta/master
 
 #transformando data.frame em data.table
 dt <- data.table::data.table(dados)
-class(dt)
+class(dt) #conferindo a classe de dt
+View(dt) #visualizando dt
+
+#exibindo as 2 primeiras linhas da coluna 3
+dt[1:2,3]
+
+#exibindo as duas primeiras linhas das colunas de 1 a 3
+dt[1:2,1:3]
+
+#exibindo as duas primeiras linhas das colunas 1, 2 e 6
+dt[1:2, c(1,2,6)]
+
+#excluindo a coluna quinzena (exibindo as duas primeiras linhas)
+dt[1:2, -"quinzena"]
+
+#excluindo a coluna quinzena e desconto_perc (exibindo as duas primeiras linhas)
+dt[1:2, -c("quinzena","desconto_perc")]
+
+#criando uma nova coluna com valor repetido em todas as linhas e exibindo as linhas 1 e 2 e colunas 6 e 7
+dt[,novacol := 0][1:2,6:7]
+
+#removendo uma coluna
+dt[, novacol := NULL][1:2]
+
+#obtendo a media e o desvio-padrão de valor_compra
+dt[,.(media = mean(valor_compra), desv_pad = sd(valor_compra))]
+
+#obtendo a soma de valor_compra, n_itens e desconto_perc
+dt[,.(s1 = sum(valor_compra), s2 = sum(n_itens), s3 = sum(desconto_perc))]
