9ffdf7647af6a70a46e550610c1b7e661e923827
Sergio Pedro <seperool@gmail.com>
2022-01-26 15:39:12 -0300

    basic_graficos - comentarios argumentos para adicionar efeitos em graficos

--- Cap7_basic_graficos.R
+++ Cap7_basic_graficos.R
@@ -80,18 +80,40 @@ turismo
 #para cor das fontes use: col.axis, col.lab, col.main, col.sub
 
 #border =
+#especifica a cor da borda de uma coluna no grafico
 
 #font =
+#controla o estilo da fonte de:
+#0 - normal
+#1 - italico
+#2 - negrito
+#3 - italico e negrito
 
 #cex =
+#controla o tamanho da fonte do textos, valor padrão é 1
+#(cex.axis, cex.lab, cex.main, cex.sub)
 
 #lty =
+#especifica o tipo de linha
+#1 - solida
+#2 - tracejada
+# ...
 
 #lwd =
+#especifica a espessura da linha (1, 2, ...)
 
 #pch =
+#controla o tipo de simbolo (1 a 25 ou especificado entre aspas)
 
 #type =
+#especifica o tipo de plotagem:
+#'p' (pontos)
+#'l' (linhas)
+#'b' (pontos conectados por linhas)
+#'o' (idem a 'b' com pontos sobrepostos a linha)
+#'h' (linhas verticais)
+#'s' (degrau no qual o dado é representado no topo da linha vertical)
+#'S' (idem ao 's' porem o dado é representado na base da linha vertical)
 
 #xlim = (inicio,fim)
 #controla os limites do eixo x
@@ -100,18 +122,38 @@ turismo
 #controla os limites do eixo y
 
 #xlab = ""
+#adiciona rotulo para o eixo x de acordo com texto entre aspas
 
 #ylab = ""
+#adiciona rotulo para o eixo y de acordo com texto entre aspas
 
 #las = ""
+#controla a orientação dos rotulos dos eixos:
+#0 - paralelo ao eixo
+#1 - horizontal
+#2 - perpendicular
+#3 - vertical
 
 #xaxt ou yaxt = ""
+#se xaxt = "n", o eixo x é definido porem não é desenhado,
+#se yaxt = "n", o eixo y é definido porem não é desenhado
 
 #text(x, y, 'texto', cex, col)
+#adiciona texto ao grafico na coordenada (x,y) 
+#podendo ser dimininuido o tamanho da fonte na proporção desejada
+#em relação ao tamanho padrão 1
+#e com a cor especificada
 
 #legend(x, y, legenda)
+#adiciona uma legenda no ponto (x,y)
+#com os simbolos dados no campo legenda
 
 #locator (n,type = 'n',...)
+#retorna coordenadas correspondentes pedidas pelo usuario ao clickar (n vezes)
+#no grafico
+#também desenha simbolos (type = 'p') ou linhas (type = '1')
+#respeitando os parametros do grafico
+#por padrão type = 'n'
 
 #segments(x0,y0, x1, y1,....)
 #desenha segmentos de linha a partir do ponto (x0,y0) ate (x1,y1)
