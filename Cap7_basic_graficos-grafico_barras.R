ad7db35febcaa22d6b3995c458c2315c3d126f53
Sergio Pedro <seperool@gmail.com>
2022-01-28 15:33:01 -0300

    basic_graficos - grafico de barras na posição vertical - estados abreviados

--- Cap7_basic_graficos-grafico_barras.R
+++ Cap7_basic_graficos-grafico_barras.R
@@ -47,3 +47,33 @@ barplot(y, names.arg = x,
 dev.off()
 
 #---
+
+Est <- c("AM", "BA", "CE", "DF", "MS", "MG", "OUTROS", "PA", "PR", "PE",
+         "RN", "RS", "RJ", "SC", "SP")
+cheg <- c(34720, 142803, 91648, 68540, 43891, 54480, 40749,
+          16877, 791396, 70259, 40488, 810670, 1164187, 195708, 2110427)
+Est
+cheg
+
+#scrip para criar um grafico de barras
+
+#as categorias devem estar ordenadas
+x <- Est[order(cheg)] #linka Est a ordem cheg
+y <- sort(cheg)/1000 #reordena cheg e divide por mil
+x
+y
+
+#definindo parametros para a janela grafica
+
+#mar = margens do grafico em relação a margem
+#mai = margem em polegada
+par(mar = c(5,4,4,2), mai = c(1,0.8,0.8,0.4))
+
+barplot(y, names.arg = x,
+        main = "Chegadas de turistas ao Brasil em 2012",
+        ylab = "Chegadas por mil",
+        xlab = "Estados",
+        cex.main = 1.5,
+        cex.names = 1,
+        axisnames = T, #inclui os nomes das categorias no eixo x
+        las = 2)
