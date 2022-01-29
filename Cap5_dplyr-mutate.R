2300cbac95598d3687009148022ac69a0bcba0b8
Sergio Pedro <seperool@gmail.com>
2022-01-29 19:51:11 -0300

    dplyr - atraves de mutate é possivel criar novas colunas na base de dados

---
+++ Cap5_dplyr-mutate.R
@@ -0,0 +1,21 @@
+# Importação de dados pelo import dataset
+View(vendas1)
+
+# Leitura de dados de arquivo em URL
+dados = read.csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados)
+dados1 = readr::read_csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados1)
+dados2 = read.csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados2)
+dados3 = readr::read_csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados3)
+
+#dplyr
+#Manipulando dados com o dplyr
+#mutate
+  #atraves de mutate é possivel criar novas colunas na base de dados
+library(magrittr)
+library(dplyr)
+dados1 %>%
+  mutate(vmci = round(valor_compra/n_itens))
