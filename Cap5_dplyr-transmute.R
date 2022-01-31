24e67f36f69c7bd3b2cfbc3c296088779db80f42
Sergio Pedro <seperool@gmail.com>
2022-01-31 20:46:49 -0300

    dplyr - faz o mesmo que mutate, mas não adiciona a nova variavel as colunas originais

---
+++ Cap5_dplyr-transmute.R
@@ -0,0 +1,21 @@
+# Importação de dados pelo import dataset
+View(vendas1)
+
+# Leitura de dados de arquivo em URL
+dados = read.csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados)
+dados1 = readr::read_csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados1)
+dados2 = read.csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados2)
+dados3 = readr::read_csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados3)
+
+#dplyr
+#Manipulando dados com o dplyr
+#transmute
+  #faz o mesmo que mutate, mas não adiciona a nova variavel as colunas originais
+library(magrittr)
+library(dplyr)
+dados1 %>%
+  transmute(vmci = valor_compra/n_itens)
