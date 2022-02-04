adb6e08aa880d027d75c7d52d9e87c655652e110
Sergio Pedro <seperool@gmail.com>
2022-02-04 14:36:00 -0300

    tibble - add coluna na tabela tibble

---
+++ Cap5_tibble-add_columm_part1.R
@@ -0,0 +1,19 @@
+# Importação de dados pelo import dataset
+View(vendas1)
+
+# Leitura de dados de arquivo em URL
+dados = read.csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados)
+dados1 = readr::read_csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados1)
+dados2 = read.csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados2)
+dados3 = readr::read_csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados3)
+
+#tibble
+#adicionar coluna: add_column
+library(magrittr)
+library(dplyr)
+dados1 %>%
+  tibble::add_column(adicionada = 1:23)
