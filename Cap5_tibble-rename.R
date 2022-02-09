82c9e931492a6ab52cc34e1df265f68b3a69f015
Sergio Pedro <seperool@gmail.com>
2022-02-08 21:53:27 -0300

    tibble - rename - renomeando uma coluna

---
+++ Cap5_tibble-rename.R
@@ -0,0 +1,19 @@
+# Importação de dados pelo import dataset
+View(vendas1)
+
+# Leitura de dados de arquivo em URL
+dados = read.csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados)
+dados1 = readr::read_csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados1)
+dados2 = read.csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados2)
+dados3 = readr::read_csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados3)
+
+#tibble
+#renomeando uma coluna: rename
+library(magrittr)
+library(dplyr)
+dados %>%
+  rename(venda = valor_compra)
