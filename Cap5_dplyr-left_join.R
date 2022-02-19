43387926867bbb20530c21ba41e2d2be0dd88eeb
Sergio Pedro <seperool@gmail.com>
2022-02-19 11:17:58 -0300

    dplyr - left_join - combinando tabelas de dados pela esquerda, o resultado final sera toda tabela da esqueda com correspondente a direita

---
+++ Cap5_dplyr-left_join.R
@@ -0,0 +1,33 @@
+# Importação de dados pelo import dataset
+View(vendas1)
+
+# Leitura de dados de arquivo em URL
+dados = read.csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados)
+dados1 = readr::read_csv2(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas.csv")
+head(dados1)
+dados2 = read.csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados2)
+dados3 = readr::read_csv(file = "https://raw.githubusercontent.com/Lucianea/Alta/master/vendas1.csv")
+head(dados3)
+
+#tibble
+#Combinando tabela de dados
+#left_join & right_join
+#left_join
+#a tabela final será o resultado de todas as colunas da tabela a esquerda
+#que possuam pelo menos uma coluna em comum, a coluna chave
+library(magrittr)
+library(dplyr)
+x = dados1 %>%
+  select(cupom, filial, valor_compra) %>%
+  filter(valor_compra > 500)
+x
+y = dados1 %>%
+  select(filial, n_itens) %>%
+  filter(n_itens < 8)
+y
+left_join(x,y)
+#a coluna filial é a coluna chave
+#ao juntar x e y pela esqueda, a tabela final resultou nas colunas de x que possuem correspondentes em y
+#a base da junção é a tabela x
