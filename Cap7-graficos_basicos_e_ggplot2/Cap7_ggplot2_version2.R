60374fc384c28998c05e1a51673ff8f37d987dfb
seperool <seperool@gmail.com>
2023-05-23 01:14:23 -0300

    ggplot2 - Inserindo texto no gráfico versão 2

---
+++ Cap7-graficos_basicos_e_ggplot2/Cap7_ggplot2_version2.R
@@ -0,0 +1,30 @@
+#ggplot2
+
+#Biblioteca
+library(ggplot2) #Elaboração de gráficos
+library(readr) #Leitura de dados
+library(tibble) #Estruturação de data.frame
+
+#Importação de dados
+dados <- read_csv("/home/sergio/Programacao/R/Dados/Dados_de_importacao/turismo.csv")
+View(dados)
+
+#Transformando dados em tibble
+Turismo <- tibble(dados)
+class(Turismo)
+is_tibble(Turismo)
+
+#Plotando gráfico
+p <- ggplot(data = Turismo, aes(x=cheg_2012/1000,y=cheg_2013/1000)) #Salva gráfico em variável
+
+#Aplicando elementos de texto na forma janela em branco
+p +
+  geom_blank() + #Produz efeito janela em branco
+  ggtitle("Título",subtitle = "Subtítulo") +
+  xlab("Eixo x") +
+  ylab("Eixo y") +
+  labs(caption = "Elaborado por ...") +
+  theme_bw(base_size = 18)
+
+#fechando janela gráfica
+dev.off()
