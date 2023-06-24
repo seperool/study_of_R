a534fed403434a044ea67d3699ad3e205165dd37
seperool <seperool@gmail.com>
2023-06-24 06:25:17 -0300

    Paletas de cores do pacote RColorBrewer

---
+++ Cap7-graficos_basicos_e_ggplot2/ggplot2/Cap7_ggplot2-Paleta_Greys_RColorBrewer.R
@@ -0,0 +1,7 @@
+#script para visualizar a paleta greys (escala de cinza) do pacote RColorBrewer
+
+#Biblioteca
+library(RColorBrewer)
+
+#Visualizando paleta
+display.brewer.pal(n = 10, name = "Greys")
---
+++ Cap7-graficos_basicos_e_ggplot2/ggplot2/Cap7_ggplot2-Paletas_RColorBrewer.R
@@ -0,0 +1,7 @@
+#script para visualizar as paletas de cores do pacote RColorBrewer
+
+#Biblioteca
+library(RColorBrewer)
+
+#Visualizando paletas
+display.brewer.all()
