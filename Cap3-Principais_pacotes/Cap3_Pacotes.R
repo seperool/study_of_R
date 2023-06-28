3a5ebc2d9d5d9146cb3dbbfed52f4431967dc4a6
seperool <seperool@gmail.com>
2023-06-28 03:33:25 -0300

    Atualização comentários

--- Cap3-Principais_pacotes/Cap3_Pacotes.R
+++ Cap3-Principais_pacotes/Cap3_Pacotes.R
@@ -1,31 +1,31 @@
-install.packages("stringr") # Pacote para trabalhar com strings, texto
-install.packages("Rmarkdown") # Pacote para fazer relatorio, rmd
+install.packages("stringr") #Pacote para trabalhar com strings, texto
+install.packages("Rmarkdown") #Pacote para fazer relatorio, rmd
 install.packages("distill") #Autor: url, affiliation e affiliation_url
-install.packages("knitr") # Interpretação e compilação do documento rmd, formato tabela kable
-install.packages("kableExtra") # Mais opções de formatação de tabelas kable
-install.packages("pander") # Converte R em pandoc, para gerar .md
-install.packages("xtable") # Ajuda a fazer tabela
-install.packages("data.table") # Manipulação de base de dados DataFrame
-install.packages("janitor") # Limpeza de dados
+install.packages("knitr") #Interpretação e compilação do documento rmd, formato tabela kable
+install.packages("kableExtra") #Mais opções de formatação de tabelas kable
+install.packages("pander") #Converte R em pandoc, para gerar .md
+install.packages("xtable") #Ajuda a fazer tabela
+install.packages("data.table") #Manipulação de base de dados DataFrame
+install.packages("janitor") #Limpeza de dados
 install.packages("DescTools") #
-install.packages("tidyverse") # Conjunto de pacotes para leitura, manipulação, exploração e compartilhamento de dados
-#ggplot2 (elaboração de gráficos)
-#tibble (estruturação de data.frame) 
-#tidyr (organização de dados)
-#readr (leitura de dados)
-#purrr (programação funcional)
-#magrittr (operador pipe "%>%", concatena linhas de comando)
-#dplyr (manipulação de dados)
-install.packages("foreign") # Leitura de arquivos com extensão minitab, Octave, ...
+install.packages("tidyverse") #Conjunto de pacotes para leitura, manipulação, exploração e compartilhamento de dados
+#ggplot2 (Elaboração de gráficos)
+#tibble (Estruturação de data.frame) 
+#tidyr (Organização de dados)
+#readr (Leitura de dados)
+#purrr (Programação funcional)
+#magrittr (Operador pipe "%>%", concatena linhas de comando)
+#dplyr (Manipulação de dados)
+install.packages("foreign") #Leitura de arquivos com extensão minitab, Octave, ...
 
 #Pacotes auxiliares ggplot2
-install.packages("ggthemes") # Temas adicionais para graficos
+install.packages("ggthemes") #Temas adicionais para gráficos
 install.packages("grid") #
-install.packages("RColorBrewer") # Pacote com paleta de cores
-install.packages("extrafont") # Opções de fontes
+install.packages("RColorBrewer") #Pacote com paleta de cores para gráficos
+install.packages("extrafont") #Opções de fontes de texto para gráficos
 install.packages("showtext")
 
-# Conectores com banco de dados
+#Conectores com banco de dados
 install.packages("DBI")
 install.packages("odbc")
 install.packages("RPostgres") # quebrado!
