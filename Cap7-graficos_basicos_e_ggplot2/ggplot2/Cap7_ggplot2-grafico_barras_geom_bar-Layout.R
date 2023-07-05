c60cdc6bd54beed1c9167d599f9828a2724ab999
seperool <seperool@gmail.com>
2023-07-05 14:44:14 -0300

    Add comentários

--- Cap7-graficos_basicos_e_ggplot2/ggplot2/Cap7_ggplot2-grafico_barras_geom_bar-Layout.R
+++ Cap7-graficos_basicos_e_ggplot2/ggplot2/Cap7_ggplot2-grafico_barras_geom_bar-Layout.R
@@ -35,45 +35,45 @@ View(dt)
 
 #Plotagem gráfico de barras com duas ou mais categorias
 p = ggplot(dt)+
-  aes(x=reorder(Estado,chegada), y=chegada/1000, fill = ano)+
-  geom_bar(stat = "identity",position = "dodge")+
+  aes(x=reorder(Estado,chegada), y=chegada/1000, fill = ano)+ #Mapeamento das variáveis
+  geom_bar(stat = "identity",position = "dodge")+ #Forma de barras
   geom_text(aes(label = round(chegada/1000,2)), 
             position = position_dodge(width = 0.9), 
-            vjust=-0.25)
+            vjust=-0.25) #Insere valores sobre as barras
 
 #Adicionando camadas a p
 p1 = p +
   labs(title = "Chegada de Turistas ao Brasil - Versão barras lado a lado",
        x = "Estados",
        y = "Número de chegadas por mil")+
-  theme_bw(base_size = 18)+
-  theme(plot.title = element_text(hjust = 0.5))+
-  scale_fill_grey(start = 0, end = 0.9)
+  theme_bw(base_size = 18)+ #Insere tema
+  theme(plot.title = element_text(hjust = 0.5))+ #Centraliza o título
+  scale_fill_grey(start = 0, end = 0.9) #Ajusta a escala de cinza
 
 p1
 
 #Plotando versão barras empilhadas
 p = ggplot(dt)+
-  aes(x=reorder(Estado,chegada), y=chegada/1000, fill = ano)+
-  geom_bar(stat = "identity")+
+  aes(x=reorder(Estado,chegada), y=chegada/1000, fill = ano)+ #Mapeamento das variáveis
+  geom_bar(stat = "identity")+ #Forma de barras
   geom_text(aes(label = round(chegada/1000,2)), 
-            position = position_stack(vjust=1))
+            position = position_stack(vjust=1)) #Insere valores sobre as barras
 
 #Adicionando camadas a p
 p2 = p+
-  labs(title = "Chegada de Turistas ao Brasil - Versão barras empilhadas",
-       x = "Estados",
-       y = "Número de chegadas por mil")+
-  theme_bw(base_size = 18)+
-  theme(plot.title = element_text(hjust = 0.5))+
-  scale_fill_grey(start = 0.4, end = 1)
+  labs(title = "Chegada de Turistas ao Brasil - Versão barras empilhadas", #Título
+       x = "Estados", #Eixo x
+       y = "Número de chegadas por mil")+ #Eixo y
+  theme_bw(base_size = 18)+ #Insere tema
+  theme(plot.title = element_text(hjust = 0.5))+ #Centraliza o título
+  scale_fill_grey(start = 0.4, end = 1) #Ajusta a escala de cinza
 
 p2
 
 #Layout (patchwork)
 pp = p1 + p2 + plot_layout(ncol = 1) +
   plot_annotation(title = "Gráfico de Barras com duas ou mais categorias",
-                  tag_levels = "1")
+                  tag_levels = "1") #Título geral e número em cada gráfico
 
 pp
 
