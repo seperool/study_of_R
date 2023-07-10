90ebc7ffcd98fae23ecccc647e9988240b1eb8fe
seperool <seperool@gmail.com>
2023-07-10 04:52:39 -0300

    Add Comentários no código

--- Cap7-graficos_basicos_e_ggplot2/ggplot2/Cap7_ggplot2-Histograma_geom_histogram.R
+++ Cap7-graficos_basicos_e_ggplot2/ggplot2/Cap7_ggplot2-Histograma_geom_histogram.R
@@ -21,15 +21,17 @@ p = ggplot(dados,aes(x=cheg_2013/1000))
 
 #Adição de camadas
 p+
-  geom_histogram(aes(y= ..count.., fill=factor(Regiao)),
-                 position = "identity", alpha = 0.6,binwidth = 0.1)+
-  scale_x_log10()+
+  geom_histogram(aes(y= ..count.., fill = factor(Regiao)), #fill = variável factor
+                 position = "identity", #Histogramas sobrepostos
+                 alpha = 0.6, #Densidade das cores
+                 binwidth = 0.1)+ #Largura das caixas (barras)
+  scale_x_log10()+ #Eixo x em escala logarítmica
   labs(x = "Chegadas em escala logarítmica",
        y = "Frequência Absoluta",
        title = "Histograma do número de chegadas de turistas ao Brasil \nAno de 2013")+
-  theme_bw(base_size = 18)+
-  scale_fill_discrete(name = "Região")+
-  scale_fill_grey(start = 0.2,end = 0.8)
+  theme_bw(base_size = 18)+ #Tipo de tema
+  scale_fill_discrete(name = "Região")+ #Escala de cores dos dados discretos
+  scale_fill_grey(start = 0.2,end = 0.8) #Escala de cinza
 
 #Fechando dispositivo gráfico
 dev.off()
