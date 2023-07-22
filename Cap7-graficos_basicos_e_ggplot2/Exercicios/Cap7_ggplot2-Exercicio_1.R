8fd8eaa95ee3c3ced69523e16c03baee46a04694
seperool <seperool@gmail.com>
2023-07-22 07:34:02 -0300

    Add comentários

--- Cap7-graficos_basicos_e_ggplot2/Exercicios/Cap7_ggplot2-Exercicio_1.R
+++ Cap7-graficos_basicos_e_ggplot2/Exercicios/Cap7_ggplot2-Exercicio_1.R
@@ -21,24 +21,27 @@ str(dados)
 
 #Tratamento de dados
 dt <- dados %>% 
-  filter(Estado == c("SaoPaulo","RioJaneiro","RioGrandeSul")) %>% 
+  filter(Estado == c("SaoPaulo","RioJaneiro","RioGrandeSul")) %>% #Filtro por Estados SP, RJ e RS
   group_by(Estado) %>% 
   summarise(cheg_2014 = sum(cheg_2014), cheg_2015 = sum(cheg_2015)) %>% 
-  pivot_longer(cols = c(cheg_2014, cheg_2015), names_to = "Ano", values_to = "Chegadas")
+  #Soma as chegadas de cada Ano, respeitando o agrupamento por Estado
+  pivot_longer(cols = c(cheg_2014, cheg_2015), 
+               names_to = "Ano", values_to = "Chegadas") #Transforma tabela larga em longa
 View(dt)
 
 #Plotagem
 ggplot(dt, aes(x=Ano, y=Chegadas/1000, fill= Estado))+
-  geom_bar(stat = "identity",position = "dodge")+
+  #Adiciona camada fill (legenda)
+  geom_bar(stat = "identity",position = "dodge")+ #Barras lado a lado
   labs(title = "Gráfico de Barras",
        subtitle = "Chegadas ao Brasil por estado e ano",
        x = "Ano",
        y = "Chegadas")+
-  theme_bw(base_size = 18)+
-  theme(axis.text.x = element_text(angle = 90))+
-  theme(plot.title = element_text(hjust = 0.5))+
-  theme(plot.subtitle = element_text(hjust = 0.5))+
-  scale_fill_grey()
+  theme_bw(base_size = 18)+ #Tema "black and white" e tamanho da fonte
+  theme(axis.text.x = element_text(angle = 90))+ #Angulo do rôtulo do eixo x
+  theme(plot.title = element_text(hjust = 0.5))+ #Título centralizado
+  theme(plot.subtitle = element_text(hjust = 0.5))+ #Subtítulo centralizado
+  scale_fill_grey() #Escala de cinza na camada fill (legenda)
 
 #Fechando dispositivo gráfico
 dev.off()
