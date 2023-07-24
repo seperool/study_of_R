4b5074f72c9c0009e7964192077d3d5d904a9feb
seperool <seperool@gmail.com>
2023-07-24 08:02:07 -0300

    janitor - clean.names()

---
+++ Cap8-Limpeza_dados/Cap8_janitor-clean_names.R
@@ -0,0 +1,23 @@
+#janitor
+
+#Limpando nomes do data.frame - clean_names():
+#Retorna nomes somente com letras em caixa baixa e com "_" como separador.
+#Manipula caracteres especiais e espaços.
+#Inclui números para nomes duplicados.
+#Converte o símbolo "%" para "percent" preservando o sentido.
+
+#Bibliotecas
+library(janitor)
+
+#Dados
+dfp <- as.data.frame(matrix(ncol = 6))
+names(dfp) <- c("OriGem",
+                "REPETE",
+                "REPETE",
+                "% de acertos",
+                "R!$@$&*",
+                "")
+dfp
+
+#Limpando nomes
+clean_names(dfp)
