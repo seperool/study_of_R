e21aec5a0e61972fb5d415329ca3dc97b3353b11
seperool <seperool@gmail.com>
2023-07-26 13:09:11 -0300

    mice - Substituição dos valores perdidos

--- Cap3-Principais_pacotes/Cap3_Pacotes.R
+++ Cap3-Principais_pacotes/Cap3_Pacotes.R
@@ -7,6 +7,7 @@ install.packages("pander") #Converte R em pandoc, para gerar .md
 install.packages("xtable") #Ajuda a fazer tabela
 install.packages("data.table") #Manipulação de base de dados DataFrame
 install.packages("janitor") #Limpeza de dados
+install.packages("mice") #Substituir valores perdidos
 install.packages("DescTools") #
 install.packages("devtools")
 install.packages("tidyverse") #Conjunto de pacotes para leitura, manipulação, exploração e compartilhamento de dados
