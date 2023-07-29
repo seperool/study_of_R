1138bf78b386e37a066223da04ce9b928412dc49
seperool <seperool@gmail.com>
2023-07-29 09:39:34 -0300

    janitor - Produzindo tabelas de frequência para uma variável - tabyl

--- Cap8-Limpeza_dados/Cap8_janitor-tabyl.R
+++ Cap8-Limpeza_dados/Cap8_janitor-tabyl.R
@@ -1,4 +1,13 @@
-#valid_percent
+#janitor - tabyl
+
+#tabyl é uma versão melhorada da função table() do pacote básico.
+#Retorna dataframe que pode ser melhorado e impresso com kable(), do pacote knitr.
+#Calcula porcentagens automaticamente (porcentagem total e porcentagem valida, sem NA).
+#Pode opcionalmente exibir valores NA.
+#Quando NA ocorre, uma coluna adicional valid_percent (porcentagem valida) é adicionada.
+#Pode opcionalmente ordernar as contagens (frequência).
+#Pode ser usado com operador pipe %>%, do pacote magrittr.
+#Quando a variável for do tipo categórica, os valores perdidos são contabilizados na tabela.
 
 #Biblioteca
 library(janitor)
@@ -11,7 +20,7 @@ z <- c(NA,1:7,NA,NA)
 vazia <- rep(NA,10)
 
 #Tabela de frequência da variável x
-tabyl(x, sort = TRUE)
+tabyl(x, sort = TRUE) #Ordena a variável x
 
 tabyl(x) %>% 
-  select(x, valid_percent)
+  select(x, valid_percent) #Seleciona as colunas x e valid_percent
