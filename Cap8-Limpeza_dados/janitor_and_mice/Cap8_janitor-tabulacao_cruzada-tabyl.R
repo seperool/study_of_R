19745eafde3b0d4d3c1838cf5190a94fb50350e5
seperool <seperool@gmail.com>
2023-08-01 11:31:49 -0300

    janitor - Tabulação cruzada- tabyl - adornos

--- Cap8-Limpeza_dados/janitor_and_mice/Cap8_janitor-tabulacao_cruzada-tabyl.R
+++ Cap8-Limpeza_dados/janitor_and_mice/Cap8_janitor-tabulacao_cruzada-tabyl.R
@@ -1,5 +1,7 @@
 #janitor - tabyl
-#Tabulação cruzada
+#Tabulação cruzada - gera frequência da coluna x em y.
+
+
 
 #Biblioteca
 library(janitor)
@@ -25,3 +27,37 @@ dt
 #Tabulação cruzada entre x e y
 #frequência de y em x
 tabyl(dt,x,y)
+
+#Tabulação cruzada entre x e y com adornos
+tabyl(dt,x,y) %>% 
+  adorn_percentages("row") %>% #Inclui porcentagens com base na linha
+  adorn_pct_formatting() #Formata para %
+
+tabyl(dt,x,y) %>% 
+  adorn_percentages("col") %>% #Inclui porcentagens com base na coluna
+  adorn_pct_formatting() #Formata para %
+
+tabyl(dt,x,y) %>% 
+  adorn_percentages("all") %>% #Inclui porcentagens com base no total geral
+  adorn_pct_formatting() #Formata para %
+
+tabyl(dt,x,y, show_na = F) %>%  #Exclui NA
+  adorn_percentages("all") %>% #Inclui porcentagens com base no total geral
+  adorn_rounding(2) %>% #Arredonda com 2 casas decimais
+  adorn_ns() %>% #Inclui contagens
+  adorn_title("combined", 
+              row_name = "Cor de selo",
+              col_name = "Categorias") #Adiciona títulos
+
+#Arredondamentos
+#half up - arredonda para cima
+dt %>% 
+  tabyl(x,y) %>% 
+  adorn_percentages() %>% 
+  adorn_rounding(digits = 0,rounding = "half up")
+
+#half to even - arredonda para baixo
+dt %>% 
+  tabyl(x,y) %>% 
+  adorn_percentages() %>% 
+  adorn_rounding(digits = 0,rounding = "half to even")
