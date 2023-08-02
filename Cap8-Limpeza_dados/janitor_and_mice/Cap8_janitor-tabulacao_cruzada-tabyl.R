7d4d827b51376624ececf30127869039bb8a5065
seperool <seperool@gmail.com>
2023-08-02 11:48:57 -0300

    janitor - Tabulação cruzada - tabyl - Descrição e teoria

--- Cap8-Limpeza_dados/janitor_and_mice/Cap8_janitor-tabulacao_cruzada-tabyl.R
+++ Cap8-Limpeza_dados/janitor_and_mice/Cap8_janitor-tabulacao_cruzada-tabyl.R
@@ -1,13 +1,30 @@
 #janitor - tabyl
-#Tabulação cruzada - gera frequência da coluna x em y.
+#Tabulação cruzada - Gera frequência da coluna x em y.
 
+#Tabulação cruzada
+##Cada célula na tabela representando uma combinação exclusiva de valores
+##entre as colunas x e y (frequências).
 
+#Uma tabulação cruzada é gerada com a função tabyl().
+
+#Propriedades:
+##Retorna um dataframe.
+
+##Calcula frequências absolutas, 
+##mas é possível incluir frequências relativas por linha ou coluna.
+
+##Também é possível incluir as frequências em forma de porcentagem.
+
+##Pode (opcionalmente) mostrar os valores NA ("show_na = F" exclui NA).
+
+##A função tabyl(), do pacote janitor, produz resultado que só seria possível através
+##de um conjunto de funções do sistema tidyverse (dplyr e tidyr).
 
 #Biblioteca
 library(janitor)
 library(dplyr)
 
-#Criando uma tabela
+#Criando uma tabela - data.frame
 cor_selo <- c("azul",
               "preto",
               "azul",
