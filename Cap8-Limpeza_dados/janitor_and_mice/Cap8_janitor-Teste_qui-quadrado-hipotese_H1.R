9ed6b113fb005957ba568dbbafe46ef8837e4107
seperool <seperool@gmail.com>
2023-08-06 00:53:55 -0300

    janitor - Teste qui-quadrado - comentado

---
+++ Cap8-Limpeza_dados/janitor_and_mice/Cap8_janitor-Teste_qui-quadrado-hipotese_H1.R
@@ -0,0 +1,48 @@
+#janitor - Teste qui-quadrado - Hipótese H_1
+
+#Hipóteses:
+##H_0
+##A variável linha é independente da variável coluna, ou seja, 
+##a proporção no total das linhas deve ser a mesma quando considerada
+##em cada categoria da variável linha.
+
+##H_1
+##A variável linha é dependente da variável coluna, ou seja,
+##a proporção no total das linhas é diferente da proporção
+##em cada categoria da variável linha.
+
+#Testes:
+##o nível de significância (alpha) adotado na pesquisa, 
+##que em geral costuma ser de 0,05(ou seja, 5%).
+
+##Se p-valor > 0,05 = H_0
+##Não é possível rejeitar a hipótese nula (H_0).
+##Concluímos que as variáveis não são dependentes, são independentes,
+##ao nível de significância de 5%.
+
+##Se p-valor <= 0,05 = H_1
+##Rejeitamos a hipótese conhecida como nula (H_0).
+##Concluímos que as variáveis são dependentes ao nível de significância de 5%.
+
+#Bibliotecas
+library(magrittr) #Operador pipe "%>%", concatena linhas de comando
+library(janitor) #Limpeza de dados
+
+#Suponha dois grupos que receberam tratamento
+grupo <- c(rep("A",15),rep("B",15))
+set.seed(20)
+resposta <- c(sample(0:1,16,replace = T),rep(1,14))
+dt = data.frame(grupo,resposta)
+
+tab = tabyl(dt, grupo, resposta, show_na= F)
+tab
+
+#Aplicando o teste qui-quadrado
+chisq.test(tab)
+
+#Conclusão
+##p-value = 0.006706
+##Logo, p-value <= alpha
+##Assim, rejeitamos a hipótese nula (H_0),
+##há uma associação estatisticamente significativa entre as variáveis.
+##É a hipótese H_1.
