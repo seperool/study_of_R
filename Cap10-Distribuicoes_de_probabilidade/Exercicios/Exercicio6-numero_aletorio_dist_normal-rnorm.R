1efbc6632d06dffe4d91d4b0bd2a6f03892e9141
seperool <seperool@gmail.com>
2023-11-12 15:29:45 -0300

    Exercicio 6 - comentários

--- Cap10-Distribuicoes_de_probabilidade/Exercicios/Exercicio6-numero_aletorio_dist_normal-rnorm.R
+++ Cap10-Distribuicoes_de_probabilidade/Exercicios/Exercicio6-numero_aletorio_dist_normal-rnorm.R
@@ -20,18 +20,20 @@ y <- rnorm(20,0,1)
 y
 
 #Gráfico comparativo
-PlotFdist(y,
-          args.curve = list(expr = "dnorm(x,mean(y),sd(y))",
-                            col = "black"),
-          args.dens = list(col = "gray"),
-          args.ecdf = NA,
-          xlim = c(-4,4))
+PlotFdist(y, #vetor dados da variável
+          args.curve = list(expr = "dnorm(x,mean(y),sd(y))", #dados da distribuição normal
+                            col = "black"), #Distribuição normal em preto
+          args.dens = list(col = "gray"), #grafico densidade em cinza
+          args.boxplot = NA, #Omitir boxplot
+          args.ecdf = NA, #Omitir distribuição acumulada
+          xlim = c(-4,4)) #Limites do eixo x
+
 #Organizando legenda
 legend(x = "topright",
-       legend = c("kernel density",
-                  expression(X%~%N(mu==0, sigma==1))),
-       fill = c("gray80","black"),
-       text.width = 0.2)
+       legend = c("kernel density", #Camada 1 - densidade
+                  expression(X%~%N(mu==0, sigma==1))), #Camada 2 - função distribuição normal
+       fill = c("gray80","black"), #Cor das camadas na legenda
+       text.width = 0.2) #tamanho caixa da legenda
 
 #Fechando dispositivo gráfico
 dev.off()
